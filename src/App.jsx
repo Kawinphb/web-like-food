b58168210189e794abf0571fa1f73c4ba43b0d11
Kawinphb <kawinphong.b@gmail.com>
2024-06-13 15:24:48 +0700

    Update link to show list page
    fix Show page no found

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './App.css'
-import MainPage from './component/page/MainPage'
-import ShowFavoritePage from './component/page/ShowFavoritePage'
+import "./App.css";
+import MainPage from "./component/page/MainPage";
+import ShowFavoritePage from "./component/page/ShowFavoritePage";
 
 function App() {
-  
-
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<MainPage/>}></Route>
-      <Route path="/list-favorite" element={<ShowFavoritePage/>}></Route>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<MainPage />}></Route>
+        <Route path="/list-favorite" element={<ShowFavoritePage />}></Route>
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/component/layout/Navbar.jsx
+++ src/component/layout/Navbar.jsx
@@ -1,5 +1,6 @@
 import RestaurantIcon from "@mui/icons-material/Restaurant";
 import SearchBox from "../search-box/SearchBox";
+import { Link } from "react-router-dom";
 function Navbar({ searchItem, setDisplay, iniData }) {
   return (
     <nav className="bg-white border-gray-200 pt-8 px-36 ">
@@ -24,29 +25,29 @@ function Navbar({ searchItem, setDisplay, iniData }) {
             >
               <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white">
                 <li>
-                  <a
-                    href="/"
+                  <Link
+                    to="/"
                     className="block py-2 px-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0"
                     aria-current="page"
                   >
                     Home
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a
-                    href="#"
+                  <Link
+                    to="#"
                     className="block py-2 px-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 "
                   >
                     About
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a
-                    href="/list-favorite"
+                  <Link
+                    to="/list-favorite"
                     className="block py-2 px-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0"
                   >
                     Your favorite
-                  </a>
+                  </Link>
                 </li>
               </ul>
             </div>
